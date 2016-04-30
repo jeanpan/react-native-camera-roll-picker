1035c0248ae7c59916efcab04942f41e8f048721
jeanpan <beats.1213@gmail.com>
2016-04-30 18:16:14 -0400

    update README and add keywords

--- Example/index.ios.js
+++ Example/index.ios.js
@@ -8,20 +8,65 @@ import React, {
 
 import CameraRollPicker from 'react-native-camera-roll-picker';
 
-class Example extends Component {
-  render() {
+const Example =  React.createClass ({
+  getInitialState: function() {
+    return {
+      num: 0,
+    };
+  },
+
+  getSelectedImages: function(images) {
+    var num = images.length;
+    // console.log(images);
+    this.setState({
+      num: num,
+    });
+  },
+
+  render: function() {
     return (
       <View style={styles.container}>
-        <CameraRollPicker />
+        <View style={styles.content}>
+          <Text style={styles.text}>
+            <Text style={styles.bold}> {this.state.num} </Text> images has been selected
+          </Text>
+        </View>
+        <CameraRollPicker
+          groupTypes='SavedPhotos'
+          batchSize={25}
+          maximum={15}
+          assetType='Photos'
+          imagesPerRow={3}
+          imageMargin={5}
+          callback={this.getSelectedImages} />
       </View>
     );
   }
-}
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 20,
+    backgroundColor: '#F6AE2D',
+  },
+  content: {
+    marginTop: 15,
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+  text: {
+    fontSize: 16,
+    alignItems: 'center',
+    color: '#fff',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  info: {
+    fontSize: 12,
   },
 });
 
--- README.md
+++ README.md
@@ -1,14 +1,38 @@
 # react-native-camera-roll-picker
 CameraRoll Picker component for React native
 
-##Installation:
+##Add to Project
+
+Install component through npm
 ```
-npm install react-native-camera-roll-picker --save
+$ npm install react-native-camera-roll-picker --save
 ```
 
-##Example:
+Require component
+```
+var CameraRollPicker = require('react-native-camera-roll-picker');
+```
+
+##Basic Usage
 ```js
-<CameraRollPicker />
+<CameraRollPicker
+  callback={this.getSelectedImages} />
 ```
 
-##Props:
+##Props
+- `callback` : Callback function when images was selected.
+- `groupTypes` : The group where the photos will be fetched, one of 'Album', 'All', 'Event', 'Faces', 'Library', 'PhotoStream' and 'SavedPhotos'. Default is *SavedPhotos*
+- `assetType` : The asset type, one of 'Photos', 'Videos' or 'All'. Default is *Photos*
+- `batchSize` : Number of images per fetch. Default is *25*
+- `maximum` : Maximum number of selected images. Default is *15*
+- `imagesPerRow` : Number of images per row. Default is *3*
+- `imageMargin` : Margin size of one image. Default is *5*
+
+##Run Example
+```
+$ git clone https://github.com/jeanpan/react-native-camera-roll-picker.git
+$ cd react-native-camera-roll-picker
+$ cd Example
+$ npm install
+$ react-native run-ios
+```
