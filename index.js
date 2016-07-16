951d459385835fec6bf25fd40b9209db46c1d3c7
jeanpan <beats.1213@gmail.com>
2016-07-16 13:55:24 +0800

    fix the bug of disappearing marker when loading more images & add loading indicator

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-import React,{Component} from 'react'
+import React, {Component} from 'react'
 import {
-  CameraRoll
-  ,Image
-  ,Platform
-  ,StyleSheet
-  ,View
-  ,Text
-  ,Dimensions
-  ,TouchableOpacity
-  ,ListView
+  CameraRoll,
+  Image,
+  Platform,
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  ListView,
+  ActivityIndicator,
 } from 'react-native'
 
 import SGListView from 'react-native-sglistview'
@@ -20,104 +21,198 @@ class CameraRollPicker extends Component{
     this.state = {
       images: [],
       selected: [],
-      dataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+      lastCursor: null,
+      loadingMore: false,
+      noMore: false,
+      dataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}),
     };
   }
-  componentWillMount() {
-    var { width } = Dimensions.get('window');
-    this.imageSize = ((width - (this.props.imagesPerRow+1) * this.props.imageMargin) / this.props.imagesPerRow);
 
-    //Fetch
+  componentWillMount() {
+    var {width} = Dimensions.get('window');
+    var {imageMargin, imagesPerRow} = this.props;
+
+    this._imageSize = (width - (imagesPerRow + 1) * imageMargin) / imagesPerRow;
+
+    this.fetch();
+  }
+
+  fetch() {
+    if (!this.state.loadingMore) {
+      this.setState({loadingMore: true}, () => { this._fetch(); });
+    }
+  }
+
+  _fetch() {
+    var {batchSize, groupTypes, assetType} = this.props;
+
     var fetchParams = {
-      first: 1000,
-      groupTypes: this.props.groupTypes,
-      assetType: this.props.assetType,
+      first: batchSize,
+      groupTypes: groupTypes,
+      assetType: assetType,
     };
 
-    if (Platform.OS === "android") delete fetchParams.groupTypes;
-    if (this.state.lastCursor) fetchParams.after = this.state.lastCursor;
+    if (Platform.OS === "android") {
+      // not supported in android
+      delete fetchParams.groupTypes;
+    }
+
+    if (this.state.lastCursor) {
+      fetchParams.after = this.state.lastCursor;
+    }
 
     CameraRoll.getPhotos(fetchParams)
-      .then((data) => {
-        //split to rows
-        var rows=[];
-        while (data.edges.length > 0){
-            rows.push(data.edges.splice(0,this.props.imagesPerRow));
-        }
-        //cloneWithRows
-        this.setState({dataSource: this.state.dataSource.cloneWithRows(rows)})
-      });
+      .then((data) => this._appendImages(data), (e) => console.log(e));
   }
 
+  _appendImages(data) {
+    var assets = data.edges;
+    var newState = {
+      loadingMore: false,
+    };
+
+    this.setState({
+      loadingMore: false,
+    });
+
+    if (!data.page_info.has_next_page) {
+      newState.noMore = true;
+    }
+
+    if (assets.length > 0) {
+      newState.lastCursor = data.page_info.end_cursor;
+      newState.images = this.state.images.concat(assets);
+      newState.dataSource = this.state.dataSource.cloneWithRows(
+        this._nEveryRow(newState.images, this.props.imagesPerRow)
+      );
+    }
+
+    this.setState(newState);
+  }
+
+  render() {
+    var {imageMargin, backgroundColor} = this.props;
+    return (
+      <View
+        style={[styles.wrapper, {padding: imageMargin, paddingRight: 0, backgroundColor: backgroundColor},]}>
+        <SGListView
+          style={{flex: 1,}}
+          renderFooter={this._renderFooterSpinner.bind(this)}
+          onEndReached={this._onEndReached.bind(this)}
+          dataSource={this.state.dataSource}
+          renderRow={rowData => this._renderRow(rowData)} />
+      </View>
+    );
+  }
+
+  _renderImage(item) {
+    var {selectedMarker, imageMargin} = this.props;
+
+    var marker = selectedMarker ? selectedMarker :
+      <Image
+        style={[styles.marker, {width: 25, height: 25, right: imageMargin + 5},]}
+        source={require('./circle-check.png')}
+      />;
+
+    return (
+      <TouchableOpacity
+        key={item.node.image.uri}
+        style={{marginBottom: imageMargin, marginRight: imageMargin}}
+        onPress={event => this._selectImage(item.node.image)}>
+        <Image
+          source={{uri: item.node.image.uri}}
+          style={{height: this._imageSize, width: this._imageSize}} >
+          { (this.state.selected.indexOf(item.node.image) >= 0) ? marker : null }
+        </Image>
+      </TouchableOpacity>
+    );
+  }
+
+  _renderRow(rowData) {
+    var items = rowData.map((item) => {
+      if (item === null) {
+        return null;
+      }
+      return this._renderImage(item);
+    });
+
+    return (
+      <View style={styles.row}>
+        {items}
+      </View>
+    );
+  }
+
+  _renderFooterSpinner() {
+    if (!this.state.noMore) {
+      return <ActivityIndicator style={styles.spinner} />;
+    }
+    return null;
+  }
+
+  _onEndReached() {
+    if (!this.state.noMore) {
+      this.fetch();
+    }
+  }
 
   _selectImage(image) {
-    var selected = this.state.selected;
-    var index = selected.indexOf(image);
+    var {maximum, imagesPerRow, callback} = this.props;
+
+    var selected = this.state.selected,
+        index = selected.indexOf(image);
 
     if (index >= 0) {
       selected.splice(index, 1);
     } else {
-      if (selected.length < this.props.maximum) selected.push(image);
-      else selected = [image];
+      if (selected.length < maximum) {
+        selected.push(image);
+      }
     }
-    this.setState({ selected: selected });
-    this.props.callback(this.state.selected);
-  }
-  render(){
-    return (
-      <View style={[ styles.wrapper, { padding: this.props.imageMargin, paddingRight: 0, backgroundColor: this.props.backgroundColor}, ]}>
-        <SGListView
-          style={styles.list}
-          contentContainerStyle={styles.listContainer}
-          dataSource={this.state.dataSource}
-          renderRow={rowData => this.renderRow(rowData)} />
-      </View>
-    );
-  }
-  renderRow(data){
-    var selectedMarker = this.props.selectedMarker ?
-                          this.props.selectedMarker
-                          :
-                          <Image
-                            style={[ styles.checkIcon, { width: 25, height: 25, right: this.props.imageMargin + 5 }, ]}
-                            source={require('./circle-check.png')}
-                          />
 
-    var items=[];
-    data.forEach(item =>{
-      items.push(
-        <TouchableOpacity
-          style={{marginBottom: this.props.imageMargin, marginRight: this.props.imageMargin}}
-          onPress={event => this._selectImage(item.node.image)}>
-          <Image
-            source={{uri: item.node.image.uri}}
-            style={{height: this.imageSize, width: this.imageSize}} >
+    this.setState({
+      selected: selected,
+      dataSource: this.state.dataSource.cloneWithRows(
+        this._nEveryRow(this.state.images, imagesPerRow)
+      ),
+    });
 
-            { (this.state.selected.indexOf(item.node.image) >= 0)? selectedMarker : null }
-
-          </Image>
-        </TouchableOpacity>
-      )
-    })
-    return(
-      <View style={styles.row}>
-        {items}
-      </View>
-    )
+    callback(this.state.selected);
   }
+
+  _nEveryRow(data, n) {
+    var result = [],
+        temp = [];
+
+    for (var i = 0; i < data.length; ++i) {
+      if (i > 0 && i % n === 0) {
+        result.push(temp);
+        temp = [];
+      }
+      temp.push(data[i]);
+    }
+
+    if (temp.length > 0) {
+      while (temp.length !== n) {
+        temp.push(null);
+      }
+      result.push(temp);
+    }
+
+    return result;
+  }
+
 }
 
 const styles = StyleSheet.create({
   wrapper:{
     flex: 1,
   },
-  listContainer: {
-    flexDirection: 'column',
-  },
   row:{
     flexDirection: 'row',
+    flex: 1,
   },
-  checkIcon: {
+  marker: {
     position: 'absolute',
     top: 5,
     backgroundColor: 'transparent',
@@ -140,14 +235,17 @@ CameraRollPicker.propTypes = {
     'Videos',
     'All',
   ]),
+  batchSize: React.PropTypes.number,
   imagesPerRow: React.PropTypes.number,
   imageMargin: React.PropTypes.number,
   callback: React.PropTypes.func,
   selectedMarker: React.PropTypes.element,
   backgroundColor: React.PropTypes.string,
 }
+
 CameraRollPicker.defaultProps = {
   groupTypes: 'SavedPhotos',
+  batchSize: 10,
   maximum: 15,
   imagesPerRow: 3,
   imageMargin: 5,
